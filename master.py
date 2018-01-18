d0bdebfa13f879df5d50a34512027a248d8a7253
Aditya Singh <32441247+UneasierSine@users.noreply.github.com>
2018-01-18 08:56:04 -0500

    Create master.py

---
+++ master.py
@@ -0,0 +1,2 @@
+//in this file, the co-processor (Jetson or RPi) will listen in on the SmartDashboard where the OI will display if automated controls are
+//pressed, and the co-processor will then execute a file with the necessary algorithm for automation
