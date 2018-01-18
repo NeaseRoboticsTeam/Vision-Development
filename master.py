b4e8b43f045475f09d0a54dd510190e69832d48b
Aditya Singh <32441247+UneasierSine@users.noreply.github.com>
2018-01-18 09:10:48 -0500

    Update master.py

--- master.py
+++ master.py
@@ -1,2 +1,24 @@
 //in this file, the co-processor (Jetson or RPi) will listen in on the SmartDashboard where the OI will display if automated controls are
 //pressed, and the co-processor will then execute a file with the necessary algorithm for automation
+
+//import libraries and modules
+import sys
+import time
+import logging
+import cv3
+from networktables import NetworkTables
+
+//setup logging
+logging.basicConfig(level=logging.DEBUG)
+
+//setup ip address
+if len(sys.argv) != 2:
+    print("Error: specify an IP to connect to!")
+    exit(0)
+ip = sys.argv[1]
+
+//initializations and setup
+NetworkTables.initialize(server=ip)
+sd = NetworkTables.getTable("SmartDashboard")
+camera = cv3.VideoCapture(0)
+
