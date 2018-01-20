3e648b7537abce5e295ca2c0224246584799f20b
UneasierSine <aditya.s530593@gmail.com>
2018-01-20 17:33:31 -0500

    finished up a rough version of getCubeTrajectory

--- master.py
+++ master.py
@@ -25,8 +25,26 @@ camera = cv2.VideoCapture(0)
 while true:
     #if the placeholder1 button is pressed
     if sd.getString("Automation_Controls") == "Get_Power_Cube_Trajectory":
-        getCubeTrajectory()
+        centroidOfCorners[1] = getCubeTrajectory()
+        sd.putString("Automation_Controls", "Co-Processor Reply: Function Executed")
+        sd.putString("Go To (X,Y)", str(centroidofCorners[0]) + str(centroidOfCorners[1]))
 
 def getCubeTrajectory():
     ret, frame = camera.read()
     corners = cv2.cornerHarris(frame, 5, 4, 0.04)
+    corner = 0
+    xVals[2]
+    yVals[2]
+    for x in range (0, corners.width):
+        for y in range (corners.height, corners.height/2):
+            if frame(x, y) == [255, 255, 0]:
+                if corners(x, y) > 0.5:
+                    corner = corner + 1
+                    xVals[counter-1] = x
+                    yVals[counter-1] = y            
+        if corner = 3:
+            break
+    averages[1]
+    averages[0] = (xVals[1] + xVals[2] + xVals[3])/3
+    averages[1] = (yVals[1] + yVals[2] + yVals[3])/3
+    return averages
