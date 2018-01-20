15ba66a52cb4072fd8a476c82df548c0d2c15bef
UneasierSine <aditya.s530593@gmail.com>
2018-01-20 15:28:36 -0500

    fix comments from // to # because it is python, not a logical programming language

--- master.py
+++ master.py
@@ -1,29 +1,29 @@
-//in this file, the co-processor (Jetson) will listen in on the SmartDashboard where the OI will display if automated controls are
-//pressed, and the co-processor will then execute a file with the necessary algorithm for automation
+#in this file, the co-processor (Jetson) will listen in on the SmartDashboard where the OI will display if automated controls are
+#pressed, and the co-processor will then execute a file with the necessary algorithm for automation
 
-//import libraries and modules
+#import libraries and modules
 import sys
 import time
 import logging
 import cv3
 from networktables import NetworkTables
 
-//setup logging
+#setup logging
 logging.basicConfig(level=logging.DEBUG)
 
-//setup ip address
+#setup ip address
 if len(sys.argv) != 2:
     print("Error: specify an IP to connect to!")
     exit(0)
 
-//initializations and setup
+#initializations and setup
 NetworkTables.initialize(server='roborio-6468-frc.local')
 sd = NetworkTables.getTable("SmartDashboard")
 camera = cv3.VideoCapture(0)
 
-//repeat this action forever
+#repeat this action forever
 while true:
-    //if the placeholder1 button is pressed
-    if sd.getBoolean("placeholder1") == true:
-        //do blah
-      
+    #if the placeholder1 button is pressed
+    if sd.getBoolean("placeholder1"s) == true:
+        #do blah
+        sd.getBoolean("placeholder1")
