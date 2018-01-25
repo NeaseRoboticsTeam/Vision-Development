6587767cb32c1a6d5f16c7a677ff5aa26041cb9c
UneasierSine <aditya.s530593@gmail.com>
2018-01-24 21:57:26 -0500

    Create a file for testing NetworkTables communication with the RoboRIO.

---
+++ TestNetworkTables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "ntcore.h"
+
+int main()
+{
+	auto inst = nt::GetDefaultInstance();
+
+	nt::NetworkTableInstance::StartClient(inst, "VisionServer", kDefaultPort);
+	nt::NetworkTableInstance::GetTable("SmartDashboard");
+
+	while(true)
+	{
+		String response;
+		cin<<"Give Permission to Drive?"<<response<<end;
+		if(response == "yes") nt::NetworkTableInstance::putString("Permission", "Yes");
+		if(response == "no") nt::NetworkTableInstance::putString("Permission", "No");
+	}
+}
