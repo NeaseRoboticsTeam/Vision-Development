59056c9b5fc62ad22f00a4b550ad91e6182b0847
Aditya Singh <32441247+UneasierSine@users.noreply.github.com>
2018-02-08 16:38:59 -0500

    Update sockets.cpp

--- sockets.cpp
+++ sockets.cpp
@@ -8,12 +8,23 @@
 
 int main()
 {
+  int socket_desc;
   struct sockaddr_in server;
+  
+  char * buffer;
+  
   socket_desc = socket(AF_INET , SOCK_STREAM , 0);
   
   server.sin_addr.s_addr = inet_addr("127.0.0.1");
   server.sin_family = AF_INET;
   server.sin_port = htons(444);
   
-  connect(socket_desc , (struct sockaddr *)&server , sizeof(server)
+  connect(socket_desc , (struct sockaddr *)&server , sizeof(server);
+  
+  while(true)
+  {
+    if(read(socket_desc, buffer, 2000, 0))
+    {
+    }
+  }
 }
