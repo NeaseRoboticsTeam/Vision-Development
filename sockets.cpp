d57f1755ceec4afb739448aaffd2dfb7b14df5b3
Aditya Singh <32441247+UneasierSine@users.noreply.github.com>
2018-02-09 08:56:15 -0500

    Update sockets.cpp

--- sockets.cpp
+++ sockets.cpp
@@ -9,21 +9,26 @@ int main()
   int socket_desc;
   struct sockaddr_in server;
   
-  char * buffer;
-  
+  //create socket client
   socket_desc = socket(AF_INET , SOCK_STREAM , 0);
   
+  //server settings
   server.sin_addr.s_addr = inet_addr("127.0.0.1");
   server.sin_family = AF_INET;
   server.sin_port = htons(444);
   
+  //connect to server socket
   connect(socket_desc , (struct sockaddr *)&server , sizeof(server);
-  
+  //create buffer variable for print data
+  bzero(buffer, 256);
+          
   while(true)
   {
-    if(read(socket_desc, buffer, 2000, 0) != 0)
+    //read and save to buffer variable
+    if(read(socket_desc, buffer, 255) >= 0)
     {
-      send(socket_desc, "Message Received!", strlen("Message Recieved!"), 0);
+      //send the recieved message
+      send(socket_desc, "Message Recieved!", strlen("Message Recieved!"), 0);
     }
   }
 }
