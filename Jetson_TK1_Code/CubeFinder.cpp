3ca83024ad909db646689c0bc7c86bb57f548f6f
Aditya Singh <32441247+UneasierSine@users.noreply.github.com>
2018-02-15 16:33:03 -0500

    Update CubeFinder.cpp

--- Jetson_TK1_Code/CubeFinder.cpp
+++ Jetson_TK1_Code/CubeFinder.cpp
@@ -6,6 +6,7 @@ https://www.chiefdelphi.com/forums/showthread.php?t=112694
 
 @author Christian
 @author Aditya
+@author Rishabh
 */
 
 /*import important modules where
@@ -58,11 +59,26 @@ int main()
 public void getCube (VideoCapture &sensor)
 {
 	//set up the depth and rgb images as well as the matrix to store the corners
- 	Mat rgb, depth, corners;
+ 	Mat rgb, depth;
 	int height = sensor->get(CV_CAP_PROP_FRAME_HEIGHT);
 	int width = sensor->get(CV_CAP_PROP_FRAME_WIDTH);
 		
 	depth = sensor->retrieve(rgb, CAP_OPENNI_DEPTH_MAP);
 	rgb = sensor->retrieve(bgrImage, CAP_OPENNI_BGR_IMAGE);
 	
+	vector<vector<Point>> contours; // Vector for storing contour
+    	vector<Vec4i> hierarchy;
+
+    	findContours( rgb, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE ); // Find the contours in the image
+  
+     	for( int i = 0; i< contours.size(); i++ ) // iterate through each contour. 
+      	{
+       		double a=contourArea( contours[i],false);  //  Find the area of contour
+       		if(a>largest_area){
+       		largest_area=a;
+		//Store the index of largest contour
+       		largest_contour_index=i;                
+		// Find the bounding rectangle for biggest contour
+       		bounding_rect=boundingRect(contours[i]);
+       }
 }
